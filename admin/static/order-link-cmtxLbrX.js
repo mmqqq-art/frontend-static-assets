5c27c93b502b3b56574fea605fb61574b6a0261f
mmqqq-art <117814914+mmqqq-art@users.noreply.github.com>
2026-03-21 20:34:15 +0800

    chore: upload github assets from 20260321-202024

---
+++ admin/static/order-link-cmtxLbrX.js
@@ -0,0 +1,1 @@
+import{j as s,e as n}from"./index-D9x8yz26.js";import{B as i}from"./index-U_XjAANM.js";function o({orderId:r,className:a}){return r?s.jsx(i,{asChild:!0,className:`p-0 ${a||""}`,variant:"link",children:s.jsx(n,{search:{search:r},to:"/dashboard/order",children:r})}):s.jsx("span",{children:"--"})}export{o as O};
