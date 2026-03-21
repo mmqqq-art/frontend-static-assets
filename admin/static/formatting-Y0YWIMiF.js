a88218f2644173d32fb109c996648306c01a01dd
mmqqq-art <117814914+mmqqq-art@users.noreply.github.com>
2026-03-21 20:31:23 +0800

    chore: clear repository before full asset upload

--- admin/static/formatting-Y0YWIMiF.js
+++
@@ -1,1 +0,0 @@
-function r(o){if(o===0)return"0 B";const t=1024,n=["B","KB","MB","GB","TB","PB","EB","ZB","YB"],B=Math.floor(Math.log(o)/Math.log(t));return`${(o/t**B).toFixed(2)} ${n[B]}`}export{r as f};
